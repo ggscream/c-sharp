03ea9951cb3471d296fa01cb435f9cb34e0134ab
Katia <puzynia.katia@gmail.com>
2022-11-25 15:42:36 +0300

    решена первая задача

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine ("Введите первое число: ");
+double x = Convert.ToDouble(Console.ReadLine ());
+
+Console.WriteLine ("Введите второе число: ");
+double y = Convert.ToDouble(Console.ReadLine ());
+
+if (y > x)
+{
+    double max = y;
+    double min = x;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
+
+else
+{
+    double max = x;
+    double min = y;
+    Console.WriteLine($"max = {max}, min = {min}");
+}
